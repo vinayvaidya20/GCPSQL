a42a14dc0e9cbdd62496ac3889c3ae1000d5cf83
vinayvaidya20 <vinay.vaidya20@gmail.com>
2018-08-12 20:04:36 -0400

    Add files via upload

---
+++ gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/GcpDemoApplication.java
@@ -0,0 +1,12 @@
+package com.priceline.gcp.gcpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDemoApplication.class, args);
+	}
+}
