4d6e8e11201e7cb60a72bd73aab7bcb8762ac1fe
vinayvaidya20 <vinay.vaidya20@gmail.com>
2018-08-25 15:47:57 -0400

    Update HelloWorldController.java

--- gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HelloWorldController.java
+++ gcp-demo/src/main/java/com/priceline/gcp/gcpdemo/controller/HelloWorldController.java
@@ -3,6 +3,14 @@ package com.priceline.gcp.gcpdemo.controller;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +26,49 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, value = "/{name}")
 	public String getGreeting(@PathVariable String name) throws SQLException {
 		String lastNAME= "";
-	    try (Statement statement = GcpDemoApplication.getConnection().createStatement()) {
+		  String instanceConnectionName = "gcp-example-demo:us-central1:demo-sql-instance";
+
+	    // TODO: fill this in
+	    // The database from which to list tables.
+	    String databaseName = "TestDatabase";
+
+	    String username = "proxyuser";
+
+	    // TODO: fill this in
+	    // This is the password that was set via the Cloud Console or empty if never set
+	    // (not recommended).
+	    String password = "password";
+
+	    if (instanceConnectionName.equals("<insert_connection_name>")) {
+	      System.err.println("Please update the sample to specify the instance connection name.");
+	      System.exit(1);
+	    }
+
+	    if (password.equals("<insert_password>")) {
+	      System.err.println("Please update the sample to specify the mysql password.");
+	      System.exit(1);
+	    }
+
+	    //[START doc-example]
+	    
+	    String jdbcUrl = String.format(
+	        "jdbc:mysql://google/%s?cloudSqlInstance=%s"
+	            + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+	        databaseName,
+	        instanceConnectionName);
+		 connection = DriverManager.getConnection(jdbcUrl, username, password);
+	   //[END doc-example]
+
+	    try (Statement statement = connection.createStatement()) {
+	      ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+	      while (resultSet.next()) {
+	        System.out.println(resultSet.getString(1));
+	      }
+	    }
+	  }
+		
+		
+	    try (Statement statement = connection.createStatement()) {
 		      ResultSet resultSet = statement.executeQuery("select * from Persons");
 		      while (resultSet.next()) {
 		        System.out.println(resultSet.getInt(1));
@@ -33,7 +83,7 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String getResult() throws SQLException {
 	
-		return "SUCCESS"+GcpDemoApplication.getConnection();
+		return "SUCCESS"+connection;
 	}
 
 }
